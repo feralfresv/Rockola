2d37ea9af091846afec0ec67ded9023fcc9fdd6e
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-01-14 09:05:31 -0600

    primer commit

---
+++ Rockola/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Rockola.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BuscarLista(string palabra)
+        {
+            var ServicioYouTube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDOWDXSYml18hvagwa91sPt0nHhFkSpWnA",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var BuscarListaSolicitud = ServicioYouTube.Search.List("snippet");
+            BuscarListaSolicitud.Q = palabra; //Buscador
+            BuscarListaSolicitud.MaxResults = 10;
+
+            var BuscarListaRespuesta = BuscarListaSolicitud.Execute();
+
+            return PartialView("Search", BuscarListaRespuesta.Items);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
